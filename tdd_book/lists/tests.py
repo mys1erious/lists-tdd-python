659a3c0bc342a2888458c9c402cf1b3c125da6e4
mys1erious <subtletyzeros@gmail.com>
2022-08-06 18:48:48 +0200

    added FT for second item in a list

--- tdd_book/lists/tests.py
+++ tdd_book/lists/tests.py
@@ -10,9 +10,11 @@ class HomePage(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
-
         html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
+        self.assertIn('home.html', html)
+
+    def test_can_save_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
