1635f38ddec35657b3cd099548c56ec4cb6a2a80
mys1erious <subtletyzeros@gmail.com>
2022-08-06 19:08:17 +0200

    Item Model and associated migrations

--- tdd_book/lists/tests.py
+++ tdd_book/lists/tests.py
@@ -2,10 +2,12 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
+
 from .views import home_page
+from .models import Item
 
 
-class HomePage(TestCase):
+class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
@@ -18,3 +20,22 @@ class HomePage(TestCase):
     def test_can_save_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_item(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
