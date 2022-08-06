2682c1c2a98cf176aa6853802a7ed06cfa3ad1ef
mys1erious <subtletyzeros@gmail.com>
2022-08-06 15:33:31 +0200

    FT now checks we can input  a to-do item

--- tdd_book/functional_tests.py
+++ tdd_book/functional_tests.py
@@ -1,7 +1,11 @@
+import time
 import unittest
 
 from selenium import webdriver
 from unittest import TestCase
+
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
 
 
 class NewVisitorTest(TestCase):
@@ -12,16 +16,30 @@ class NewVisitorTest(TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app.
+        # She goes to check out its homepage
         self.browser.get('http://localhost:8000')
 
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(by=By.TAG_NAME, value='h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby is tying fly-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(by=By.ID, value='id_list_table')
+        rows = table.find_elements(by=By.TAG_NAME, value='tr')
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
 
         # There is still a text box inviting her to add another item
         # She enters "Use peacock feathers to make a fly" (Edith is very methodical)
