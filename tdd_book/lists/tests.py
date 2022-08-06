942ead2aab96d879dbcf5431f474feb3733bb4fa
mys1erious <subtletyzeros@gmail.com>
2022-08-06 16:07:44 +0200

    Refactored home page view to use a template

--- tdd_book/lists/tests.py
+++ tdd_book/lists/tests.py
@@ -1,4 +1,5 @@
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 from .views import home_page
@@ -10,10 +11,8 @@ class HomePage(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
+        response = self.client.get('/')
 
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        html = response.content.decode('utf8')
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
