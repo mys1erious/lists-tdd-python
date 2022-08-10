2e0e7751f4ccfef21e6c3579d59053d8d4866082
mys1erious <subtletyzeros@gmail.com>
2022-08-10 10:46:46 +0200

    fix: monkey-patch for Jinja templates to have template and context attrs when using in tests

--- tdd_book/jinja2.py
+++ tdd_book/jinja2.py
@@ -1,6 +1,23 @@
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import reverse
+from django.test import signals
+
 from jinja2 import Environment
+from jinja2 import Template as Jinja2Template
+
+
+# Monkey-patch for Jinja templates so that template and context attrs could be used in tests
+#   maybe move to signals ?
+ORIGINAL_JINJA2_RENDERER = Jinja2Template.render
+def instrumented_render(template_object, *args, **kwargs):
+    context = dict(*args, **kwargs)
+    signals.template_rendered.send(
+                            sender=template_object,
+                            template=template_object,
+                            context=context
+                        )
+    return ORIGINAL_JINJA2_RENDERER(template_object, *args, **kwargs)
+Jinja2Template.render = instrumented_render
 
 
 def environment(**options):
