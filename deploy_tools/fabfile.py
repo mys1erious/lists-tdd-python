b917c23783ff0e536fcefe3c790c763eae99659c
mys1erious <subtletyzeros@gmail.com>
2022-08-11 11:28:33 +0200

    fix: secret key handling rework

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -38,7 +38,7 @@ def _update_settings(source_folder, site_name):
     # sed(settings_path, "DEBUG = True", 'DEBUG = False')
     sed(settings_path, "ALLOWED_HOSTS =.+", f'ALLOWED_HOSTS = ["{site_name}"]')
 
-    secret_key_file = source_folder + '/config/secret_key.py'
+    secret_key_file = source_folder + '/config/settings/secret_key.py'
     if not exists(secret_key_file):
         chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
         key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
